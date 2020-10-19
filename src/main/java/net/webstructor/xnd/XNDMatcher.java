2b793c1aba962fdf3863b552caf8e623337f1027
yitbarekyohannes <yitbarek_yohannes@yahoo.com>
2020-10-19 09:41:32 +0300

    set summary at least 3 sentences and 300 characters

--- src/main/java/net/webstructor/xnd/XNDMatcher.java
+++ src/main/java/net/webstructor/xnd/XNDMatcher.java
@@ -149,7 +149,10 @@ public class XNDMatcher extends Matcher{
         	List<String> ordered = new ArrayList<String>();
         	List<String> original=TextRankSummary.spiltSentence(ex_t);                
         	List<String> unordered=TextRankSummary.getTopSentenceList(ex_t, 10);
-        	for (String s : original){
+        	if(unordered.size()<3){
+				return summary;
+			}
+			for (String s : original){
             		for (String s1 : unordered){
                 		if (s1.equals(s)){
                     			ordered.add(s1);
@@ -161,6 +164,11 @@ public class XNDMatcher extends Matcher{
 				summary += s+" ";
 
         		}
+		
+			if(summary.length()<300){
+				summary="";
+				return summary;
+			}
 		duration = System.nanoTime() - st_time;
 		return summary;
 	}
