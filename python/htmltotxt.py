8caf714f119872a9449b5c7c56d8415608776ae9
yitbarekyohannes <yitbarek_yohannes@yahoo.com>
2020-10-24 11:31:24 +0300

    remove paragraphs of some classes

--- python/htmltotxt.py
+++ python/htmltotxt.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 from bs4 import BeautifulSoup
 import requests
-
+import re
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('url', type=str)
@@ -12,7 +12,9 @@ def main(args):
     req = requests.get(args.url,headers=headers)
     soup = BeautifulSoup(req.text, "html.parser")
     summary=""		
-        
+    rm=[]
+    class_names=["header", "footer", "nav", "aside", "side", "menu", "layout","caption","figure"]
+    classes=[]    
     try:
         soup.footer.decompose()
     except:
@@ -33,20 +35,30 @@ def main(args):
     except:
         pass
 
+
+    for class_name in class_names:
+        classes+=soup.find_all(class_=re.compile(class_name))
+
+    for cont in classes:
+        rm.append(cont.p)
+
+
+
     for cont in soup.find_all("p"):
-   	temp=cont.text
-        try:
-            if temp[-1]!=".":
-                temp+=". "
-            else:
-                temp+="  "
-        except:
+        if cont not in rm:
+            temp=cont.text
+            try:
+                if temp[-1]!=".":
+                    temp+=". "
+                else:
+                    temp+="  "
+            except:
                 pass
-        summary+=temp
-        summary=summary.replace("\n"," ")
+            summary+=temp
+            summary=summary.replace("\n"," ")
+
 	
     print(summary.encode('utf-8'))
-
 if __name__ == "__main__":
     main(sys.argv[1:])
 
