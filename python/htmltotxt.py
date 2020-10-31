db6264a6bb33411cdce67fe9e47e2cc8ca6323f3
yitbarekyohannes <yitbarek_yohannes@yahoo.com>
2020-10-31 21:09:31 +0300

    fix paragraph filtering based on class

--- python/htmltotxt.py
+++ python/htmltotxt.py
@@ -13,7 +13,7 @@ def main(args):
     soup = BeautifulSoup(req.text, "html.parser")
     summary=""		
     rm=[]
-    class_names=["header", "footer", "nav", "aside", "menu", "layout","caption","figure"]
+    class_names=["header", "footer", "nav", "side", "aside", "menu", "layout","caption","figure"]
     classes=[]    
     
     for i in range(7):
@@ -47,6 +47,10 @@ def main(args):
     for class_name in class_names:
         classes+=soup.find_all(class_=re.compile(class_name))
 
+    if soup.body  in classes:
+        classes.remove(soup.body)
+    if soup.article  in classes:
+        classes.remove(soup.article)
     for cont in classes:
         rm+=cont.find_all("p")
 
@@ -76,6 +80,7 @@ def main(args):
             summary+=temp
             summary=summary.replace("\n"," ")
         pclass=""
+
     print(summary)
 if __name__ == "__main__":
     main(sys.argv[1:])
